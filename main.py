2794122951ced4b3fa3949de9d61e1afd288f617
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-06-25 11:42:05 +0200

    Combine ISS and Sunrise APIs

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import requests
+import datetime as dt
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json')
-response.raise_for_status()
+MY_LAT = 48.669102
+MY_LONG = 12.690720
 
-data = response.json()
+iss_response = requests.get(
+    url="http://api.open-notify.org/iss-now.json",
+    timeout=10
+)
+iss_response.raise_for_status()
+iss_data = iss_response.json()
 
-longitude = data['iss_position']['longitude']
-latitude  = data['iss_position']['latitude']
+iss_latitude = float(iss_data['iss_position']['latitude'])
+iss_longitude = float(iss_data['iss_position']['longitude'])
 
-iss_position = (longitude, latitude)
+iss_position = (iss_latitude, iss_longitude)
 
-print(iss_position)
+parameters = {
+    'lat': MY_LAT,
+    'lng': MY_LONG,
+    'formatted': 0,
+}
+
+ss_response = requests.get('https://api.sunrise-sunset.org/json', params=parameters)
+ss_response.raise_for_status()
+ss_data = ss_response.json()
+
+sunrise = int(ss_data['results']['sunrise'].split('T')[1].split(':')[0])
+sunset = int(ss_data['results']['sunset'].split('T')[1].split(':')[0])
+
+time_now = dt.datetime.now()
+
